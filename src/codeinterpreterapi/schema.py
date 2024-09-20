69f00c9eb3768cc412eb7ed49635074ebba623ba
KOBAYASHI Yuko <kobayashi@a-tm.co.jp>
2024-09-20 14:40:52 +0900

    Update LangChain

--- src/codeinterpreterapi/schema.py
+++ src/codeinterpreterapi/schema.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from codeboxapi.schema import CodeBoxStatus
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.pydantic_v1 import BaseModel
+from pydantic import BaseModel
 
 
 class File(BaseModel):
